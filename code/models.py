2648ce200798d86f74b62588e767c9309bc5dc4f
MatPrst <parisot.mathias.31@gmail.com>
2020-08-25 22:14:30 +0200

    add file containing all the model architectures

---
+++ code/models.py
@@ -0,0 +1,319 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net4(nn.Module):
+    def __init__(self):
+        super(Net4, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 13**2, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+
+        x = x.view(-1, 16 * 13**2)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def __str__(self):
+        return 'a4'
+
+class Net7(nn.Module):
+    def __init__(self):
+        super(Net7, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(6 * 30**2, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+
+        x = x.view(-1, 6 * 30**2)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def __str__(self):
+        return 'a7'
+
+class Net1(nn.Module):
+    def __init__(self):
+        super(Net1, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(16, 32, 5)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 4**2, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+
+        x = x.view(-1, 32 * 4**2)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def __str__(self):
+        return 'a1'
+
+class Net2(nn.Module):
+    def __init__(self):
+        super(Net2, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(16, 32, 5)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 4**2, 84)
+        self.fc2 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+
+        x = x.view(-1, 32 * 4**2)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def __str__(self):
+        return 'a2'
+
+class Net3(nn.Module):
+    def __init__(self):
+        super(Net3, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(16, 32, 5)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 4**2, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+
+        x = x.view(-1, 32 * 4**2)
+
+        x = self.fc1(x)
+        return x
+
+    def __str__(self):
+        return 'a3'
+
+class Net5(nn.Module):
+    def __init__(self):
+        super(Net5, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 13**2, 84)
+        self.fc2 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+
+        x = x.view(-1, 16 * 13**2)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def __str__(self):
+        return 'a5'
+
+class Net6(nn.Module):
+    def __init__(self):
+        super(Net6, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 13**2, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+
+        x = x.view(-1, 16 * 13**2)
+
+        x = self.fc1(x)
+        return x
+
+    def __str__(self):
+        return 'a6'
+
+class Net8(nn.Module):
+    def __init__(self):
+        super(Net8, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(6 * 30**2, 84)
+        self.fc2 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+
+        x = x.view(-1, 6 * 30**2)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def __str__(self):
+        return 'a8'
+
+class Net9(nn.Module):
+    def __init__(self):
+        super(Net9, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(6 * 30**2, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+
+        x = x.view(-1, 6 * 30**2)
+
+        x = self.fc1(x)
+        return x
+
+    def __str__(self):
+        return 'a9'
+
+class Net10(nn.Module):
+    def __init__(self):
+        super(Net10, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(16, 32, 5)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 4**2, 240)
+        self.fc2 = nn.Linear(240, 120)
+        self.fc3 = nn.Linear(120, 84)
+        self.fc4 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+
+        x = x.view(-1, 32 * 4**2)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+    def __str__(self):
+        return 'a10'
+
+class AttackNet1(nn.Module):
+    def __init__(self, in_dim=337721, out_dim=1):
+        super(AttackNet, self).__init__()
+        self.fc1 = nn.Linear(in_dim, 10)
+        self.fc2 = nn.Linear(10, out_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def __str__(self):
+        return 'attack-1'
+
+class AttackNet2(nn.Module):
+    def __init__(self, in_dim=337721, out_dim=1):
+        super(AttackNet2, self).__init__()
+        self.fc1 = nn.Linear(in_dim, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, out_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def __str__(self):
+        return 'attack-2'
+
+class AttackNet3(nn.Module):
+    def __init__(self, in_dim=337721, out_dim=1):
+        super(AttackNet3, self).__init__()
+        self.fc1 = nn.Linear(in_dim, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 10)
+        self.fc4 = nn.Linear(10, out_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+    def __str__(self):
+        return 'attack-3'
+
+class AttackNet4(nn.Module):
+    def __init__(self, in_dim=337721, out_dim=1):
+        super(AttackNet4, self).__init__()
+        self.fc1 = nn.Linear(in_dim, 20)
+        self.fc2 = nn.Linear(20, out_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def __str__(self):
+        return 'attack-4'
+
+class AttackNet5(nn.Module):
+    def __init__(self, in_dim=337721, out_dim=1):
+        super(AttackNet5, self).__init__()
+        self.fc1 = nn.Linear(in_dim, 20)
+        self.fc2 = nn.Linear(20, 20)
+        self.fc3 = nn.Linear(20, out_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def __str__(self):
+        return 'attack-5'
