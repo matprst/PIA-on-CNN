7662d3f1439e82cf3e762cc0db34b590a49f64b2
MatPrst <parisot.mathias.31@gmail.com>
2021-04-26 01:11:19 +0200

    clean attack

--- code/test_attack.py
+++ code/test_attack.py
@@ -11,20 +11,6 @@ import models
 import utils
 
 
-def get_model(architecture):
-    architectures = {
-        'a1': models.Net1,
-        'a2': models.Net2,
-        'a3': models.Net3,
-        'a4': models.Net4,
-        'a5': models.Net5,
-        'a6': models.Net6,
-        'a7': models.Net7,
-        'a8': models.Net8,
-        'a9': models.Net9
-    }
-    return architectures[architecture]
-
 def get_attack(number):
     attacks = {
         1: models.AttackNet1,
@@ -36,43 +22,114 @@ def get_attack(number):
     return attacks[number]
 
 
-device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-for i in range(1, 6):
-    models_files = sorted(glob.glob(f'models/attack_models/conv/attack-{i}*.pth'))
+# device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
+# for i in range(1, 6):
+#     models_files = sorted(glob.glob(f'models/attack_models/full/attack-{i}*.pth'))
 
-    data = {
-        'model':[],
-        'architecture':[],
-        'num_param':[],
-        'accuracy':[],
-        'precision':[],
-        'recall':[]
-    }
+#     data = {
+#         'model':[],
+#         'architecture':[],
+#         'num_param':[],
+#         'accuracy':[],
+#         'precision':[],
+#         'recall':[]
+#     }
 
-    for model in models_files:
-        filename = model.split('/')[-1]
-        architecture = filename.split('-')[2]
-        number = int(filename.split('-')[4].split('.')[0])
-        print(filename, architecture, number)
+#     for model in models_files:
+#         # model name is of the form "{attack_model}-{target_model}-{rep}"
+#         filename = model.split('/')[-1]
+#         architecture = filename.split('-')[2]
+
+#         shadow_dataset = datasets.ShadowDataset('models/shadow_models/gender/models.csv', 'models/shadow_models/gender/', split='test', architecture=architecture, fcn=False, conv=False, device=device)
+#         test_loader = DataLoader(shadow_dataset, batch_size=32, shuffle=True)
+
+#         params = utils.number_param(utils.get_model(architecture), fcn=False, conv=False)
+#         print(params)
+#         attack_model = get_attack(i)(in_dim=params).to(device)
         
-        shadow_dataset = datasets.ShadowDataset('models/shadow_models/models.csv', 'models/shadow_models/', split='test', architecture=architecture, fcn=False, conv=True, device=device)
-        test_loader = DataLoader(shadow_dataset, batch_size=32, shuffle=True)
+#         device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 
-        params = utils.number_param(get_model(architecture)(), fcn=False, conv=True)
-        print(params)
-        attack_model = get_attack(i)(in_dim=params).to(device)
-        
-        device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
+#         attack_model.load_state_dict(torch.load(model, map_location=torch.device(device)))
 
-        attack_model.load_state_dict(torch.load(model, map_location=torch.device(device)))
+#         acc, prec, recall = utils.evaluate(test_loader, attack_model, device)
+#         print(acc, prec, recall)
+#         data['model'].append(filename)
+#         data['architecture'].append(architecture)
+#         data['num_param'].append(params)
+#         data['accuracy'].append(acc)
+#         data['precision'].append(prec)
+#         data['recall'].append(recall)
 
-        acc, prec, recall = utils.evaluate(test_loader, attack_model, device)
-        data['model'].append(filename)
-        data['architecture'].append(architecture)
-        data['num_param'].append(params)
-        data['accuracy'].append(acc)
-        data['precision'].append(prec)
-        data['recall'].append(recall)
+#     df = pd.DataFrame(data)
+#     # df.to_csv('models/attack_models/conv/attack_models_conv.csv', mode='a', header=False)
 
-    df = pd.DataFrame(data)
-    # df.to_csv('models/attack_models/conv/attack_models_conv.csv', mode='a', header=False)
+def test_all_attacks(args, device):
+    for i in range(1, 6):
+        files = f"{args.attack_models_dir}/attack-{i}*.pth"
+        # models_files = sorted(glob.glob(f'models/attack_models/full/attack-{i}*.pth'))
+        models_files = sorted(glob.glob(files))
+
+        data = {
+            'model':[],
+            'architecture':[],
+            'num_param':[],
+            'accuracy':[],
+            'precision':[],
+            'recall':[]
+        }
+
+        use_only_fcn = False
+        use_only_conv = False
+        for model in models_files:
+            # model name is of the form "{attack_model}-{target_model}-{rep}"
+            filename = model.split('/')[-1]
+            architecture = filename.split('-')[2]
+
+            # shadow_dataset = datasets.ShadowDataset('models/shadow_models/gender/models.csv', 'models/shadow_models/gender/', split='test', architecture=architecture, fcn=False, conv=False, device=device)
+            shadow_dataset = datasets.ShadowDataset(
+                args.csv, 
+                args.shadow_models_dir, 
+                split='test', 
+                architecture=architecture, 
+                fcn=use_only_fcn, 
+                conv=use_only_conv, 
+                device=device
+            )
+            test_loader = DataLoader(shadow_dataset, batch_size=32, shuffle=True)
+
+            params = utils.number_param(utils.get_model(architecture), fcn=use_only_fcn, conv=use_only_conv)
+            attack_model = get_attack(i)(in_dim=params).to(device)
+            attack_model.load_state_dict(torch.load(model, map_location=torch.device(device)))
+
+            acc, prec, recall = utils.evaluate(test_loader, attack_model, device)
+            print(acc, prec, recall)
+            data['model'].append(filename)
+            data['architecture'].append(architecture)
+            data['num_param'].append(params)
+            data['accuracy'].append(acc)
+            data['precision'].append(prec)
+            data['recall'].append(recall)
+
+        df = pd.DataFrame(data)
+        # df.to_csv('models/attack_models/conv/attack_models_conv.csv', mode='a', header=False)
+        df.to_csv(args.results_csv, mode='a', header=False)
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv", type=str, default="template_output.csv", help="path to csv file that contains the info about shadow models.")
+    parser.add_argument("--shadow_models_dir", type=str, default="./models/shadow_models/", help="path to the directory that stores the states of the shadow models.")
+    parser.add_argument("--attack_models_dir", type=str, default="./models/attack_models/", help="path to the directory that stores the states of the attack models.")
+    parser.add_argument("--results_csv", type=str, default="./attacks_performance.csv", help="csv file where the performance of the attack models will be output.")
+    parser.add_argument("--cuda", action="store_true", default=False, help="use GPU.")
+
+    args = parser.parse_args()
+
+
+    device = torch.device("cuda:0" if args.cuda else "cpu")
+    test_all_attacks(args, device)
+
+
+if __name__ == "__main__":
+    main()
--- code/train_attack.py
+++ code/train_attack.py
@@ -1,41 +1,19 @@
-import time
-import PIL
-import os, sys
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
+import os
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-import torchvision
-import torchvision.transforms as transforms
-
-from torch.utils.data import Dataset, WeightedRandomSampler, DataLoader
 import torch.optim as optim
 
-import pandas as pd
-
 import datasets
-import sys
-import os
-
 import models
 import utils
 
 
-def train_attack(train_loader, val_loader, model, criterion, optimizer, epoch=10, filename='test'):
-    tic = time.time()
-    # criterion = nn.MSELoss(reduction='sum')
-    # optimizer = optim.Adam(attack_model.parameters(), lr=lr)
-    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-    # print(f'{rep} - lr={lr} - Training...')
-    losses = []
-    validate_attack(val_loader, model, device)
-    for epoch in range(epoch):
+def train_attack(dataloader, model, args, device, filename='test'):
+    criterion = nn.MSELoss(reduction='sum')
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    for epoch in range(args.epochs):
         running_loss = 0
-        for i, data in enumerate(train_loader, 0):
+        for i, data in enumerate(dataloader, 0):
             inputs, labels = data
 
             inputs = inputs.to(device)
@@ -51,125 +29,64 @@ def train_attack(train_loader, val_loader, model, criterion, optimizer, epoch=10
 
             running_loss += loss.item()
 
-        # print(f'[{epoch+1}] loss: {running_loss/(i+1):.3f}')
-        # losses.append(running_loss/(i+1))
-        acc, prec, recall = validate_attack(val_loader, model, device)
-    # print(f'\tAccuracy: {acc} %')
-    # print(f'\tPrecision: {prec} %')
-    # print(f'\tRecall: {recall} %')
-    tac = time.time()
-    # print(tac - tic)
-    # PATH = f'./models/attack_models/{filename}.pth'
-    # torch.save(model.state_dict(), PATH)
-    return acc
-
-def validate_attack(dataloader, model, device):
-    return utils.evaluate(dataloader, attack_model, device)
+        print(f'[{epoch+1}] loss: {running_loss/(i+1):.3f}')
+    path = os.path.join(args.attack_models_dir, f"{filename}.pth")
+    torch.save(model.state_dict(), path)
 
 
+def train_all_attacks(args, device):
+    architectures = [
+        models.Net1(),
+        models.Net2(),
+        models.Net3(),
+        models.Net4(),
+        models.Net5(),
+        models.Net6(),
+        models.Net7(),
+        models.Net8(),
+        models.Net9(),
+    ]
 
-device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-print(device)
+    attack_models = [
+        models.AttackNet1,
+        models.AttackNet2,
+        models.AttackNet3,
+        models.AttackNet4,
+        models.AttackNet5
+    ]
 
-architectures = [
-    models.Net1(),
-    models.Net2(),
-    models.Net3(),
-    models.Net4(),
-    models.Net5(),
-    models.Net6(),
-    models.Net7(),
-    models.Net8(),
-    models.Net9(),
-]
+    for attack in attack_models:
+        for target_model in architectures:
+            for rep in range(args.reps):
+                shadow_dataset = datasets.ShadowDataset(args.csv, args.shadow_models_dir, split='train', architecture=str(target_model))
+                dataloader = DataLoader(shadow_dataset, batch_size=32, shuffle=True)
 
-attack_models = [
-    models.AttackNet1,
-    # models.AttackNet2,
-    # models.AttackNet3,
-    # models.AttackNet4,
-    # models.AttackNet5,
-    # models.AttackNet6
-]
-
-EPOCHS = 10
-REPS = 10
-lrs = [0.005, 0.001, 0.0005]
-batch_sizes = [16, 32, 64]
-losses = ['MSE', 'L1']
-optimizers = ['adam', 'SGD']
-activations = ['relu', 'tanh', 'sigmoid']
+                params = utils.number_param(target_model)
+                attack_model = attack(in_dim=params).to(device)
+                filename = f'{attack_model}-{target_model}-{rep}'
+                print(filename)
+                train_attack(dataloader, attack_model, args=args, device=device, filename=filename)
 
 
-data = {
-    'target-model': [],
-    'lr': [],
-    'batch-size': [],
-    'loss-function': [],
-    'optimizer': [],
-    'rep': [],
-    'activation': [],
-    'accuracy': []
-}
 
-tic = time.time()
-try:
-    for rep in range(6, REPS):
-        for attack in attack_models:
-            for target_model in architectures:
-                for lr in lrs:
-                    for batch_size in batch_sizes:
-                        for loss_name in losses:
-                            if loss_name == 'MSE':
-                                loss = nn.MSELoss
-                            elif loss_name == 'L1':
-                                loss = nn.L1Loss
-                            else:
-                                raise Exception('wrong loss name')
+def main():
+    import argparse
 
-                            for opt_name in optimizers:
-                                if opt_name == 'adam':
-                                    opt = optim.Adam
-                                elif opt_name == 'SGD':
-                                    opt = optim.SGD
-                                else:
-                                    raise Exception('wrong optimizer name')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv", type=str, default="template_output.csv", help="path to csv file that contains the info about shadow models.")
+    parser.add_argument("--shadow_models_dir", type=str, default="./models/shadow_models/", help="path to the directory that stores the states of the shadow models.")
+    parser.add_argument("--attack_models_dir", type=str, default="./models/attack_models/", help="path to the directory that stores the states of the attack models.")
+    parser.add_argument("--epochs", type=int, default=10, help="number of training epochs")
+    parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
+    parser.add_argument("--reps", type=int, default=30, help="number of attack models to train for each shadow architecture.")
+    parser.add_argument("--cuda", action="store_true", default=False, help="use GPU.")
 
-                                for activation in activations:
-                                    print(f"target_model={str(target_model)}, {lr=}, {batch_size=}, {loss_name=}, {opt_name=}, {activation=}, {rep=}")
-                                    shadow_dataset_train = datasets.ShadowDataset('models/shadow_models/models.csv', 'models/shadow_models/', split='train', architecture=str(target_model))
-                                    shadow_dataset_val = datasets.ShadowDataset('models/shadow_models/models.csv', 'models/shadow_models/', split='valid', architecture=str(target_model))
-                                    dataloader_train = DataLoader(shadow_dataset_train, batch_size=batch_size, shuffle=True)
-                                    dataloader_val = DataLoader(shadow_dataset_val, batch_size=batch_size, shuffle=True)
+    args = parser.parse_args()
 
-                                    params = utils.number_param(target_model)
-                                    attack_model = attack(in_dim=params, activation=activation).to(device)
-                                    filename = f'{attack_model}-{target_model}-{rep}'
-                                    # print(filename)
 
-                                    criterion = loss(reduction='sum')
-                                    optimizer = opt(attack_model.parameters(), lr=lr)
-                                    
-                                    acc = train_attack(dataloader_train, dataloader_val, attack_model, criterion, optimizer, EPOCHS, filename)
-                                    # validate_attack(dataloader_val, attack_model)
-                                    data['target-model'].append(str(target_model))
-                                    data['lr'].append(lr)
-                                    data['batch-size'].append(batch_size)
-                                    data['loss-function'].append(loss_name)
-                                    data['optimizer'].append(opt_name)
-                                    data['rep'].append(rep)
-                                    data['activation'].append(activation)
-                                    data['accuracy'].append(acc)
-except:
-    df = pd.DataFrame.from_dict(data)
-    df.to_csv('./attack_tuning3.csv', mode='a', header=False)
-    print(df)
-    tac = time.time()
-    print(tac - tic)
-    sys.exit()
-                                
-df = pd.DataFrame.from_dict(data)
-df.to_csv('./attack_tuning3.csv', mode='a', header=False)
-print(df)
-tac = time.time()
-print(tac - tic)
+    device = torch.device("cuda:0" if args.cuda else "cpu"))
+    train_all_attacks(args, device)
+
+
+if __name__ == "__main__":
+    main()
